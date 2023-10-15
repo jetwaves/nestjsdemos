fb4538dabb50fab7508865ff4164080c03cfb94e
Shiqiang SHI <jetwaves@gmail.com>
2023-10-15 22:49:21 +0200

    remove unused files.
    Signed-off-by: Shiqiang SHI <jetwaves@gmail.com>

--- src/game/dto/create-game.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateGameDto {
-  ref: number;
-  tries_count: number;
-  latest_feedback: number;
-}
--- src/game/dto/update-game.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGameDto } from './create-game.dto';
-
-export class UpdateGameDto extends PartialType(CreateGameDto) {
-  tries_count: number;
-  latest_feedback: number;
-}
